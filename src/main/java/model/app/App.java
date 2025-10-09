aad8aab402af92bdc8631b47ce7b400da9fcb301
Jon Hauge <116887188+jonaahauge@users.noreply.github.com>
2025-10-09 22:17:25 +0200

    opprettelse av mockdata
    Laget klasse med metode for opprettelse av mockdata for både stoppested og ruter.
    
    Oppdatert konstruktør for ruter.

--- src/main/java/model/app/App.java
+++ src/main/java/model/app/App.java
@@ -1,14 +1,27 @@
 package model.app;
 
+import model.core.Rute;
 import model.core.StoppeSted;
 
 import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("test");
-        ArrayList<StoppeSted> mockStoppeSteder = new ArrayList<StoppeSted>();
-        mockStoppeSteder.add(new StoppeSted("Fredrikstad"));
-        System.out.println(mockStoppeSteder.getFirst().getName());
+        ArrayList<StoppeSted> stoppeStedList = new mockData().createMockStoppeSted();
+        System.out.println("Stoppesteder: ");
+        for(StoppeSted stoppeSted : stoppeStedList){
+            System.out.print(stoppeSted.getName() + ", ");
+        }
+        System.out.println("\n");
+        ArrayList<Rute> ruteList = new mockData().createMockRute();
+
+        System.out.println("Ruter: ");
+        for(Rute rute : ruteList){
+            System.out.print(rute.getName() + ": ");
+            for(StoppeSted stoppeSted : rute.getStoppeSteder()){
+                System.out.print(stoppeSted.getName() + ", ");
+            }
+            System.out.println();
+        }
     }
 }
