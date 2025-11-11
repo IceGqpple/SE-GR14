124d6f412225c59673bcf66a5827bf9d8cb2c9ba
Jon Hauge <116887188+jonaahauge@users.noreply.github.com>
2025-11-11 13:17:15 +0100

    Update App.java

--- app/src/main/java/app/App.java
+++ app/src/main/java/app/App.java
@@ -1,11 +1,6 @@
 package app;
 
-import domain.MockData;
-import domain.Rute;
 import frontend_api.Frontend_Api;
-
-import java.util.ArrayList;
-
 
 public class App {
         public static void main(String[] args) {
